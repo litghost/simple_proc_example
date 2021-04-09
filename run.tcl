296669b2dbf8efe303e1f1b8749e62438ba8a2b4
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-04-09 10:12:53 -0700

    Example where proc is actually required.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- run.tcl
+++ run.tcl
@@ -3,3 +3,4 @@ yosys -import
 read_verilog test_mux.v
 procs
 write_verilog test_mux_out.v
+write_json test_mux_out.json
--- test_mux.v
+++ test_mux.v
@@ -1,5 +1,8 @@
-module test(input a, input b, input s, output c);
+module test(input a, input b, input s, output c, output r);
 
+reg r_reg = 1'b0;
+
+assign r = r_reg;
 assign c = s ? a : b;
 
 endmodule
