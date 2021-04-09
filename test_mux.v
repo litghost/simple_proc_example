0fec001d1fa5dfbcda24cc7d00c87a87b65b7264
Keith Rothman <537074+litghost@users.noreply.github.com>
2021-04-09 10:15:27 -0700

    Add a simple FF.
    Signed-off-by: Keith Rothman <537074+litghost@users.noreply.github.com>

--- test_mux.v
+++ test_mux.v
@@ -1,8 +1,11 @@
-module test(input a, input b, input s, output c, output r);
+module test(input a, input b, input s, input clk, output c, output r);
 
 reg r_reg = 1'b0;
 
 assign r = r_reg;
 assign c = s ? a : b;
+
+always @(posedge clk)
+    r_reg <= a;
 
 endmodule
